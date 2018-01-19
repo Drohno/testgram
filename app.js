15f94d4e463112c056f6204cb6dbe5e04126fd37
iDev <david.roldan.nocelo@gmail.com>
2018-01-19 20:39:54 +0100

    Conseguido al menos enviar el código de Telegram

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // app.js
 var express = require('express');  
 var app = express();  
-var server = require('http').createServer(app);  
-var io = require('socket.io')(server);
-const MTProto = require('telegram-mtproto').MTProto
+var servidor = require('http').createServer(app);  
+var io = require('socket.io')(servidor);
+const MTProto = require('telegram-mtproto').MTProto;
+var port = 4200;
 
 
 app.use(express.static(__dirname + '/node_modules'));  
@@ -11,23 +12,11 @@ app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/cliente/index.html');
 });
 
-server.listen(4200);  
+servidor.listen(port, ()=>{
+  console.log("Escuchando en http://localhost:" + port);
+});  
 
-io.on('connection', (client)=>{
-    client.on('setPhone',(data)=>{
-        phone.num = data;
-    });
-
-    client.on('setCode',(data)=>{
-        phone.code = data;
-        connect();
-    });
-});
-
-const phone = {
-  num : '+9996620001',
-  code: '22222'
-}
+const phone = {}
 
 const api = {
   layer          : 57,
@@ -35,24 +24,47 @@ const api = {
   api_id         : 85721
 }
 
-const servidor = {
+const server = {
   dev: true //We will connect to the test server.
 }           //Any empty configurations fields can just not be specified
 
-const client = MTProto({ servidor, api })
+const telegram = MTProto({ server, api });
 
-async function connect(){
-  const { phone_code_hash } = await client('auth.sendCode', {
-    phone_number  : phone.num,
-    current_number: false,
-    api_id        : 85721,
-    api_hash      : '3f93a7cd550c0599884616ed325c4427'
-  })
-  const { user } = await client('auth.signIn', {
-    phone_number   : phone.num,
-    phone_code_hash: phone_code_hash,
-    phone_code     : phone.code
-  })
+io.on('connection', (client)=>{
+    client.on('setPhone',(data)=>{
+        phone.num = data;
+        try{
+          async function conseguirPhoneCodeHash(){
+            phone.phone_code_hash = await telegram('auth.sendCode', {
+              phone_number  : phone.num,
+              current_number: false,
+              api_id        : 85721,
+              api_hash      : '3f93a7cd550c0599884616ed325c4427'
+            });
+          }
+          conseguirPhoneCodeHash().then(()=>{
+            console.log("phone_code_hash\n", phone.phone_code_hash);
+          });
+      }catch(error){
+        console.log(error);
+      }
+    });
 
-  console.log('signed as ', user)
-}
+    client.on('setCode',(data)=>{
+        phone.code = data;
+        try{
+        async function conseguirPhoneUser(){
+          phone.user = await telegram('auth.signIn', {
+            phone_number   : phone.num,
+            phone_code_hash: phone.phone_code_hash,
+            phone_code     : phone.code
+          })
+        }
+        conseguirPhoneUser().then(()=>{
+          console.log('Conectado como:\n', phone.user)
+        });
+      }catch(error){
+        console.log(error);
+      }
+    });
+});
