fa7e39b0e09178dd75dc8c59ca6adcea65f8eac4
David <david@tenthman.org>
2018-01-22 08:42:04 +0100

    Now we can login

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*import { await } from 'most';*/
+
 // app.js
 var express = require('express');  
 var app = express();  
@@ -30,42 +32,79 @@ const server = {
 
 const telegram = MTProto({ server, api });
 
+var respuesta;
+
 io.on('connection', (client)=>{
     client.on('setPhone',(data)=>{
-        phone.num = data;
-        try{
-          async function conseguirPhoneCodeHash(){
-            phone.phone_code_hash = await telegram('auth.sendCode', {
-              phone_number  : phone.num,
-              current_number: false,
-              api_id        : 85721,
-              api_hash      : '3f93a7cd550c0599884616ed325c4427'
+        phone.number = data;    
+        
+        async function verifyPhone(){
+          response = await telegram('auth.checkPhone', {
+            phone_number: phone.number
+          });
+        }
+
+        verifyPhone().then(()=>{
+          console.log('Response', response);
+          // Si el teléfono está registrado en el servidor, le enviamos el código a su Telegram ya activo
+          if(response.phone_registered){
+            async function conseguirPhoneCodeHash(){
+              response = await telegram('auth.sendCode', {
+                phone_number  : phone.number,
+                sms_type : 5,
+                api_id        : 85721,
+                api_hash      : '3f93a7cd550c0599884616ed325c4427',
+                lang_code : 'en'
+              });
+            }
+            conseguirPhoneCodeHash().then(()=>{
+              phone.phone_code_hash = response.phone_code_hash;
+              client.emit('nextStep');
+            });
+          // Otherwise, le enviamos un sms con el código de confirmación de la cuenta
+          }else{
+            async function conseguirPhoneCodeHash(){
+              response = await telegram('auth.sendCode', {
+                phone_number  : phone.number,
+                sms_type : 1,
+                api_id        : 85721,
+                api_hash      : '3f93a7cd550c0599884616ed325c4427',
+                lang_code : 'en'
+              });
+            }
+            conseguirPhoneCodeHash().then(()=>{
+              phone.phone_code_hash = response.phone_code_hash;
+              client.emit('nextStep');
             });
           }
-          conseguirPhoneCodeHash().then(()=>{
-            console.log("phone_code_hash\n", phone.phone_code_hash);
-            client.emit('nextStep');
-          });
-      }catch(error){
-        console.log(error);
-      }
+        });
     });
 
     client.on('setCode',(data)=>{
-        phone.code = data;
-        try{
-        async function conseguirPhoneUser(){
-          phone.user = await telegram('auth.signIn', {
-            phone_number   : phone.num,
-            phone_code_hash: phone.phone_code_hash,
-            phone_code     : phone.code
-          })
-        }
-        conseguirPhoneUser().then(()=>{
-          console.log('Conectado como:\n', phone.user)
-        });
-      }catch(error){
-        console.log(error);
+      phone.code = data;
+      async function conseguirPhoneUser(){
+        phone.user = await telegram('auth.signIn', {
+          phone_number   : phone.number,
+          phone_code_hash: phone.phone_code_hash,
+          phone_code     : phone.code
+        })
       }
+      conseguirPhoneUser().then(()=>{
+        console.log('Conectado como:\n', phone.user);
+        async function conseguirContactos(){
+          response = await telegram('contacts.getContacts', {});
+        }
+        conseguirContactos().then(()=>{
+          console.log('Respuesta:', response);
+          async function conseguirChat(){
+            response = await telegram('messages.getChats',{
+              chats : [4561094]
+            });
+          }
+          conseguirChat().then(()=>{
+            console.log("En teoría Chat con Isma:", response);
+          });
+        });
+      });
     });
 });
